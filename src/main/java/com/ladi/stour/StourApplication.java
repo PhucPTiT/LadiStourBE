e662f24b7fe800a4217686db8c8abf0e1df7e4d1
nddev_phuc <phucnd@stftech.com.vn>
2026-03-27 15:14:58 +0700

    push base

---
+++ src/main/java/com/ladi/stour/StourApplication.java
@@ -0,0 +1,13 @@
+package com.ladi.stour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StourApplication.class, args);
+	}
+
+}
